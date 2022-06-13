5b802748e8d752e0e76275831f7da924867c2b72
Brian <b.perez3237@gmail.com>
2022-06-13 16:56:14 -0400

    commit tree 0495364f9909d939eca305bfe50279793f1a8fb9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+    validate :clickbait_check
+
+
+    private 
+
+    def clickbait_check
+        if title != nil
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+                errors.add(:title, "not clickbaity enough")
+            end
+        end
+    end
 end
